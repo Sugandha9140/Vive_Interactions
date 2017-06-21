8e1dda38869bdcfd6a8cadd2b4960ffec7acd832
Sugandha9140 <ss9140@nyu.edu>
2017-06-21 11:33:20 +0400

    Debugging Scripts

---
+++ BatuUtil/VR_Debug.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class VR_Debug : MonoBehaviour {
+
+	public TextMeshPro tmp;
+	// Use this for initialization
+	void Start () {
+		//tmp = GameObject.FindGameObjectWithTag ("textMeshProDebug").GetComponent<TextMeshPro> ();
+	}
+
+	public void Print_VR(string message){
+		tmp.text = tmp.text + "\n" + message;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
